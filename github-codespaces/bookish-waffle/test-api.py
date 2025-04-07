e7562c70bf5cb2e08c105be8e0f2b782130b10a6
AminMstlih <aminmustolih2001@gmail.com>
2025-04-07 09:57:50 +0700

    Create test-api.py

---
+++ github-codespaces/bookish-waffle/test-api.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv("OKX_API_KEY")
+SECRET_KEY = os.getenv("OKX_SECRET_KEY")
+PASSPHRASE = os.getenv("OKX_PASSPHRASE")
+
+print(API_KEY)
